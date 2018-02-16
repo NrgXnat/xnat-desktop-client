43c8974c9673a88b12af962165a8c83763aebc83
uar-darko-ljubic <darko.ljubic@upandrunningsoftware.com>
2018-02-16 04:32:00 +0100

    import

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,42 @@
-$(document).ready(function() {
-	$(".menu").click(function(){
-	    $(".menu-dropdown").toggle();
+$(function () {
+
+	$(".menu").click(function () {
+		$(".menu-dropdown").toggle();
 	});
-	$(".user").click(function(){
-	    $(".user-dropdown").toggle();
+
+	$(".user").click(function () {
+		$(".user-dropdown").toggle();
 	});
-	$(document).click(function(){
-	  $(".menu-dropdown").hide();
-	  $(".user-dropdown").hide();
+
+	$(document).click(function () {
+		$(".menu-dropdown").hide();
+		$(".user-dropdown").hide();
 	});
-	$(".menu-holder, .user-holder").click(function(e){
-	  e.stopPropagation();
+
+	$(".menu-holder, .user-holder").click(function (e) {
+		e.stopPropagation();
 	});
-	$('.alert').alert()
+
+	$('.alert').alert();
+
+
+	$(document).on('change', ':file', function () {
+		var input = $(this),
+			numFiles = input.get(0).files ? input.get(0).files.length : 1,
+			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [numFiles, label]);
+	});
+
+	// We can watch for our custom `fileselect` event like this
+	$(':file').on('fileselect', function (event, numFiles, label) {
+		var input = $(this).parents('.input-group').find(':text'),
+			log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+		if (input.length) {
+			input.val(log);
+		} else {
+			if (log) alert(log);
+		}
+	});
+	
 });
