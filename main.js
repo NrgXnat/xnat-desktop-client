59bf2544fc783df70d872ad8e3816c0aff93240d
uar-darko-ljubic <darko.ljubic@upandrunningsoftware.com>
2018-04-04 18:18:09 +0200

    Embed JRE

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const path = require('path')
+
+fix_java_path();
+
 const mizer = require('./mizer');
 const glob = require('glob')
 const electron = require('electron')
@@ -8,6 +11,7 @@ const {app, BrowserWindow, ipcMain, shell, Tray} = electron;
 
 //SET ENV
 //process.env.NODE_ENV = 'production';
+
 
 
 const debug = /--debug/.test(process.argv[2])
@@ -114,3 +118,56 @@ ipcMain.on('redirect', (e, item) =>{
 exports.anonymize = (source, script, variables) => {
   mizer.anonymize(source, script, variables);
 };
+
+function fix_java_path() {
+  const fs = require('fs');
+  const glob = require('glob');
+
+  const _app_path = __dirname;
+  const jre_search_base = path.resolve(_app_path, '..', 'jre');
+
+  let java_config_path, java_jre_path;
+  let jvm_file, jre_search_path;
+
+  if (path.extname(_app_path) === '.asar') {
+    java_config_path = path.resolve(_app_path, '..', 'app.asar.unpacked', 'node_modules', 'java', 'build', 'jvm_dll_path.json');
+
+    if (process.platform === 'win32') {
+      jre_search_path = jre_search_base + '/**/jvm.dll';
+      jvm_file = glob.sync(jre_search_path)[0];
+      java_jre_path = path.resolve(jvm_file, '..');
+
+    } else if (process.platform === 'darwin') {
+      jre_search_path = jre_search_base + '/**/libjvm.dylib';
+      jvm_file = glob.sync(jre_search_path)[0];
+      java_jre_path = path.resolve(jvm_file, '..');
+
+    } else { // linux
+      if (process.arch === 'x64') {
+        jre_search_path = jre_search_base + '/lib/amd64/**/libjvm.so';
+      } else {
+        jre_search_path = jre_search_base + '/lib/i386/**/libjvm.so';
+      }
+      
+      jvm_file = glob.sync(jre_search_path)[0];
+      java_jre_path = path.resolve(jvm_file, '..');
+    }
+
+    /*
+    fs.writeFileSync(path.resolve(_app_path, '..', 'jvm_file.txt'), jre_search_path+"\n"+'jvm_file:'+jvm_file+"\n"+java_jre_path, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+    */
+
+    //java_jre_path = path.resolve(_app_path, '..', 'jre', 'bin', 'client');
+
+    java_jre_path = '";' + java_jre_path.replace(/\\/g, '\\\\') + '"';
+    
+    fs.writeFileSync(java_config_path, java_jre_path, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+
+  }
+}
