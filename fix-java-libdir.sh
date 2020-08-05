c6e4aad3a466a7365a1a11855e054b026911b4c8
Rick Herrick <jrherrick@wustl.edu>
2020-08-05 13:52:43 -0500

    Added notarize step to electron-build, fixes to remove read-only files in JRE and path issues for fix-java-libdir.sh

--- fix-java-libdir.sh
+++ fix-java-libdir.sh
@@ -3,6 +3,10 @@
 ostype="$(echo "${OSTYPE}" | tr '[:upper:]' '[:lower:]')"
 case ${ostype} in
     darwin*)
+        [[ ! -d /Applications/XNAT-Desktop-Client.app/Contents/Resources/jre/lib/jli ]] && {
+            mkdir -p /Applications/XNAT-Desktop-Client.app/Contents/Resources/jre/lib
+            cp -R build_resources/jre/mac/lib/jli /Applications/XNAT-Desktop-Client.app/Contents/Resources/jre/lib
+        }
         sed -i '.bak' 's#lib_dir="${jre_dir}/jli"#lib_dir="/Applications/XNAT-Desktop-Client.app/Contents/Resources/jre/lib/jli"#' node_modules/java/find_java_libdir.sh
         echo "Found Mac platform \"${OSTYPE}\", applied monkey patch to node_modules/java/find_java_libdir.sh";;
     linux*)
